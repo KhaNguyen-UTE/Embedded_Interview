ef3eb403c6f1a3b80c9166f912190e864d1be528
KhaNguyenNT <84505849+KhaNguyen-UTE@users.noreply.github.com>
2023-05-24 18:00:56 +0700

    Update Baitap_Quan_li_hoc_sinh.cpp

--- BaitapC++/Baitap_Quan_li_hoc_sinh.cpp
+++ BaitapC++/Baitap_Quan_li_hoc_sinh.cpp
@@ -1,3 +1,10 @@
+/*
+* File Name: Baitap_Quan_li_hoc_sinh.cpp
+* Author: Nguyen Tran Minh Kha
+* Date: 23/05/2023
+* Description: The program has the function of managing students from a function menu
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -18,6 +25,14 @@ typedef enum{
     YEU
 }TypeHocLuc;
 
+/*
+ * Function: Class HocSinh
+ * Description: The class represents the properties and methods of the student
+ * Input:
+ *    None
+ * Output:
+ *    None
+*/
 class HocSinh{
     private:
         uint8_t ID;
@@ -32,8 +47,10 @@ class HocSinh{
         
         void setTen(string ten);
         string getTen();
+
         void setGioiTinh(TypeGioiTinh gioitinh);
         TypeGioiTinh getGioiTinh();
+
         void setTuoi(int tuoi);
         int getTuoi();
 
@@ -49,6 +66,19 @@ class HocSinh{
         TypeHocLuc HocLuc();
 };
 
+/*
+* Function: Constructor HocSinh
+* Description: import parameters to the object of the class HocSinh( name , gioiTinh, tuoi, diemtoan, diemly, diem hoa)
+* Input:
+*    name: string
+*    gioiTinh: TypeGioiTinh
+*    tuoi: int
+*    diemtoan: float
+*    diemly: float
+*    diemhoa: float
+* Output:
+*    None
+*/
 HocSinh::HocSinh(string name, TypeGioiTinh gioitinh, int tuoi, float diemtoan, float diemly, float diemhoa){
     static uint8_t id = 100;
     HocSinh::ID = id;
@@ -61,58 +91,170 @@ HocSinh::HocSinh(string name, TypeGioiTinh gioitinh, int tuoi, float diemtoan, f
     id++;
 }
 
+/*
+ * Function: HocSinh::setTen()
+ * Description: set name for object
+ * Input:
+ *    ten: string
+ * Output:
+ *    none
+*/
 void HocSinh::setTen(string ten){
     this->NAME = ten;
 }
 
+/*
+ * Function: HocSinh::getTen()
+ * Description: use to get name
+ * Input:
+ *    none
+ * Output:
+ *    return name in type of string
+*/
 string HocSinh::getTen(){
     return HocSinh::NAME;
 }
 
+/*
+ * Function: HocSinh::setGioiTinh()
+ * Description: set sex for object
+ * Input:
+ *    gioitinh: TypeGioiTinh
+ * Output:
+ *    none
+*/
 void HocSinh::setGioiTinh(TypeGioiTinh gioitinh){
     this->GIOI_TINH = gioitinh;
 }
 
+/*
+ * Function: HocSinh::getGioiTinh()
+ * Description: use to get sex
+ * Input:
+ *    none
+ * Output:
+ *    return sex in type of TypeGioiTinh
+*/
 TypeGioiTinh HocSinh::getGioiTinh(){
     return HocSinh::GIOI_TINH;
 }
 
+/*
+ * Function: HocSinh::setTuoi()
+ * Description: set age for object
+ * Input:
+ *    tuoi: int
+ * Output:
+ *    none
+*/
 void HocSinh::setTuoi(int tuoi){
     this->TUOI = tuoi;
 }
 
+/*
+ * Function: HocSinh::getTuoi()
+ * Description: use to get age
+ * Input:
+ *    none
+ * Output:
+ *    return age in type of int
+*/
 int HocSinh::getTuoi(){
     return HocSinh::TUOI;
 }
 
+/*
+ * Function: HocSinh::DiemTrungBinh()
+ * Description: use to get average scores of 3 subjects
+ * Input:
+ *    none
+ * Output:
+ *    return average scores in type of float
+*/
 float HocSinh::DiemTrungBinh(){
     return (HocSinh::DIEM_TOAN + HocSinh::DIEM_LY + HocSinh::DIEM_HOA) / 3 ;
 }
 
+/*
+ * Function: HocSinh::setDiemToan()
+ * Description: set math scores for object
+ * Input:
+ *    diemtoan: float
+ * Output:
+ *    none
+*/
 void HocSinh::setDiemToan(float diemtoan){
     this->DIEM_TOAN = diemtoan;
 }
 
+/*
+ * Function: HocSinh::getDiemToan()
+ * Description: use to get math scores
+ * Input:
+ *    none
+ * Output:
+ *    return math scores in type of float
+*/
 float HocSinh::getDiemToan(){
     return this->DIEM_TOAN;
 }
 
+/*
+ * Function: HocSinh::setDiemLy()
+ * Description: set physics scores for object
+ * Input:
+ *    diemly: float
+ * Output:
+ *    none
+*/
 void HocSinh::setDiemLy(float diemly){
     this->DIEM_LY = diemly;
 }
 
+/*
+ * Function: HocSinh::getDiemLy()
+ * Description: use to get physic scores
+ * Input:
+ *    none
+ * Output:
+ *    return physic scores in type of float
+*/
 float HocSinh::getDiemLy(){
     return this->DIEM_LY;
 }
 
+/*
+ * Function: HocSinh::setDiemHoa()
+ * Description: set chemistry scores for object
+ * Input:
+ *    diemhoa: float
+ * Output:
+ *    none
+*/
 void HocSinh::setDiemHoa(float diemhoa){
     this->DIEM_HOA = diemhoa;
 }
 
+/*
+ * Function: HocSinh::getDiemHoa()
+ * Description: use to get chemistry scores
+ * Input:
+ *    none
+ * Output:
+ *    return chemistry scores in type of float
+*/
 float HocSinh::getDiemHoa(){
     return this->DIEM_HOA;
 }
 
+/*
+ * Function: HocSinh::HocLuc()
+ * Description: use to get academic ability of students
+ * Input:
+ *    none
+ * Output:
+ *    return academic ability in type of TypeHocLuc
+*/
 TypeHocLuc HocSinh::HocLuc(){
     if (HocSinh::DiemTrungBinh() >=8) return GIOI;
     else if (HocSinh::DiemTrungBinh() >= 6.5) return KHA;
@@ -120,9 +262,26 @@ TypeHocLuc HocSinh::HocLuc(){
     else return YEU;
 }
 
+/*
+ * Function: HocSinh::getId()
+ * Description: use to get Id of students
+ * Input:
+ *    none
+ * Output:
+ *    return Id in type of uint8_t
+*/
 uint8_t HocSinh::getId(){
     return HocSinh::ID;
 }
+
+/*
+ * Function: Class Menu
+ * Description: The class represents the properties and methods of the menu
+ * Input:
+ *    None
+ * Output:
+ *    None
+*/
 class Menu{
     private:
         vector <HocSinh> Database;
@@ -137,6 +296,14 @@ class Menu{
         void hienthiHocSinh();
 };
 
+/*
+* Function: Constructor Menu
+* Description: import parameters to the object of the class Menu with 8 functions
+* Input:
+*    none
+* Output:
+*    none
+*/
 Menu::Menu(){
     int status_menu = 0;
     do
@@ -153,6 +320,8 @@ Menu::Menu(){
             cout << "Nhap lua chon cua ban: ";
             cin >> status_menu; 
         }
+
+        /* Check condition of user option*/
         if (status_menu == 8) break;
         if (status_menu <=0 || status_menu > 7){
             status_menu = 0;
@@ -173,6 +342,7 @@ Menu::Menu(){
                         }
                         while (status == '1');
                     }break;
+
             case 2: {
                         char status;
                         do{
@@ -184,6 +354,7 @@ Menu::Menu(){
                         }
                         while (status == '1');
                     }break;
+
             case 3: {
                         char status;
                         do{
@@ -195,6 +366,7 @@ Menu::Menu(){
                         }
                         while (status == '1');
                     }break;
+
             case 4:{
                         char status;
                         do{
@@ -206,6 +378,7 @@ Menu::Menu(){
                         }
                         while (status == '1');
                     }break;
+
             case 5: {
                         char status;
                         Menu::sapxepGPA(); 
@@ -213,6 +386,7 @@ Menu::Menu(){
                         cin >> status;
                         if (status == '1') Menu::hienthiHocSinh();
                     }break;
+
             case 6: {
                         char status;
                         Menu::sapxepTen();
@@ -220,13 +394,22 @@ Menu::Menu(){
                         cin >> status;
                         if (status == '1') Menu::hienthiHocSinh();
                     }break;
+
             case 7: Menu::hienthiHocSinh(); break;
     }
     }
-    while (status_menu != 8 );
+    while (status_menu != 8 ); //loop until the user selects the 8 key
     
 }
 
+/*
+ * Function: Menu::addHocSinh()
+ * Description: The method has the function of entering student information including name, age, gender and grades
+ * Input:
+ *    none
+ * Output:
+ *    add info into Database
+*/
 void Menu::addHocSinh(){
     string ten;
     string s_GioiTinh;
@@ -274,6 +457,14 @@ void Menu::addHocSinh(){
 
 }
 
+/*
+ * Function: Menu::updateInfo()
+ * Description: The method has the ability to update student information through ID
+ * Input:
+ *    none
+ * Output:
+ *    add up date info into Database
+*/
 void Menu::updateInfo(){
     uint8_t id_update;
     cout << "Nhap Id cua sinh vien muon update:";
@@ -339,6 +530,14 @@ void Menu::updateInfo(){
     cin.ignore(100,'\n');
 }
 
+/*
+ * Function: Menu::hienthiHocSinh()
+ * Description: The method has the function of displaying student information on the screen in the form of a table
+ * Input:
+ *    none
+ * Output:
+ *    show the data
+*/
 void Menu::hienthiHocSinh(){
     cout << "+--------+-------------------------+--------+-----------+--------+--------+---------+--------+-----------+" <<endl;
     cout << "|   ID   |           Ten           |  Tuoi  | Gioi Tinh | D.Toan |  D.Ly  |  D.Hoa  |  D.TB  |  Hoc Luc  |" <<endl;
@@ -361,6 +560,14 @@ void Menu::hienthiHocSinh(){
     cout << "+--------------------------------------------------------------------------------------------------------+" <<endl;
 }
 
+/*
+ * Function: Menu::deleteHocSinh()
+ * Description: The method has the function to remove 1 student from the data
+ * Input:
+ *    none
+ * Output:
+ *    remove from the data
+*/
 void Menu::deleteHocSinh(){
     uint8_t id_delete;
     bool status = false;
@@ -381,6 +588,14 @@ void Menu::deleteHocSinh(){
 
 }
 
+/*
+ * Function: Menu::findHocSinh()
+ * Description: The method has the function of searching students in the data by ID
+ * Input:
+ *    none
+ * Output:
+ *    display information of the student being searched
+*/
 void Menu::findHocSinh(){
     uint8_t id_find;
     bool status = false;
@@ -412,6 +627,14 @@ void Menu::findHocSinh(){
     if ( status == false) cout <<"--Khong co sinh vien trong Data--" <<endl;
 }
 
+/*
+ * Function: Menu::sapxepGPA()
+ * Description: The method has the function of sorting students in ascending order by GPA
+ * Input:
+ *    none
+ * Output:
+ *    show the result
+*/
 void Menu::sapxepGPA(){
     uint8_t length = Database.size();
     for(int i = 0; i < length; i++){
@@ -427,6 +650,14 @@ void Menu::sapxepGPA(){
     }
 }
 
+/*
+ * Function: Menu::sapxepTen()
+ * Description: The method has the function of sorting students in ascending order by Name
+ * Input:
+ *    none
+ * Output:
+ *    show the result
+*/
 void Menu::sapxepTen(){
     uint8_t length = Database.size();
     for(int i = 0; i < length; i++){
@@ -442,6 +673,5 @@ void Menu::sapxepTen(){
 }
 
 int main(){
-    Menu *menu1;
     Menu();
 }
